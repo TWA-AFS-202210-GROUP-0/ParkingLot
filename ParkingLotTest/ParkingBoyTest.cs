9a33ea3e4cad7616a44a26a1a8e203970f4aed91
Jie Li <JLi147@slb.com>
2022-10-30 20:10:50 +0800

    feat: [Story1 AC5] return null when parkingBoy park given the parkingLot managed has capacity and is full

--- ParkingLotTest/ParkingBoyTest.cs
+++ ParkingLotTest/ParkingBoyTest.cs
@@ -11,7 +11,7 @@ namespace ParkingLotTest;
 public class ParkingBoyTest
 {
     [Fact]
-    public void Shoule_return_ticket_when_parkingBoy_manage_a_parklot_with_default_capacity_and_park_car_given_a_parkingBoy_and_a_car()
+    public void Should_return_ticket_when_parkingBoy_manage_a_parklot_with_default_capacity_and_park_car_given_a_parkingBoy_and_a_car()
     {
         // given
         ParkingBoy parkingBoy = new ParkingBoy();
@@ -25,7 +25,7 @@ public class ParkingBoyTest
     }
 
     [Fact]
-    public void Shoule_return_car_when_parkingBoy_get_a_volid_ticket_given_a_car_park_in_a_SingleParkingLot_managed_by_a_parkingBoy()
+    public void Should_return_car_when_parkingBoy_get_a_volid_ticket_given_a_car_park_in_a_SingleParkingLot_managed_by_a_parkingBoy()
     {
         // given
         ParkingBoy parkingBoy = new ParkingBoy();
@@ -40,7 +40,7 @@ public class ParkingBoyTest
     }
 
     [Fact]
-    public void Shoule_return_several_ticket_when_parkingBoy_manage_a_parklot_with_default_capacity_and_park_several_cars_given_SingleParkingLot_managed_by_a_parkingBoy_and_3_cars()
+    public void Should_return_several_ticket_when_parkingBoy_manage_a_parklot_with_default_capacity_and_park_several_cars_given_SingleParkingLot_managed_by_a_parkingBoy_and_3_cars()
     {
         // given
         ParkingBoy parkingBoy = new ParkingBoy();
@@ -59,7 +59,7 @@ public class ParkingBoyTest
     }
 
     [Fact]
-    public void Shoule_return_correspond_car_when_parkingBoy_fetch_with_a_vilid_ticket_given_SingleParkingLot_managed_by_a_parkingBoy_with_3_cars_parking()
+    public void Should_return_correspond_car_when_parkingBoy_fetch_with_a_vilid_ticket_given_SingleParkingLot_managed_by_a_parkingBoy_with_3_cars_parking()
     {
         // given
         ParkingBoy parkingBoy = new ParkingBoy();
@@ -82,7 +82,7 @@ public class ParkingBoyTest
     }
 
     [Fact]
-    public void Shoule_return_null_when_parkingBoy_fetch_given_a_parkingBoy_get_a_wrong_ticket()
+    public void Should_return_null_when_parkingBoy_fetch_given_a_parkingBoy_get_a_wrong_ticket()
     {
         // given
         ParkingBoy parkingBoy = new ParkingBoy();
@@ -103,7 +103,7 @@ public class ParkingBoyTest
     }
 
     [Fact]
-    public void Shoule_return_null_when_parkingBoy_fetch_given_a_parkingBoy_get_a_used_ticket()
+    public void Should_return_null_when_parkingBoy_fetch_given_a_parkingBoy_get_a_used_ticket()
     {
         // given
         ParkingBoy parkingBoy = new ParkingBoy();
@@ -121,5 +121,26 @@ public class ParkingBoyTest
         var fetchedCar2 = parkingBoy.Fetch(tickets[0]);
         // then
         Assert.Equal(null, fetchedCar2);
+    }
+
+    [Fact]
+    public void Should_return_null_when_parkingBoy_park_given_a_parkingBoy_manage_a_parklot_with_default_capacity_and_full()
+    {
+        // given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        var cars = new List<Car>();
+        for (int i = 0; i < 10; i++)
+        {
+            cars.Add(new Car());
+        }
+
+        SingleParkingLot parkingLot = new SingleParkingLot();
+        parkingBoy.Manage(parkingLot);
+        var tickets = parkingBoy.ParkSeveral(cars);
+        var car = new Car();
+        // when
+        var ticket = parkingBoy.Park(car);
+        // then
+        Assert.Equal(null, ticket);
     }
 }
