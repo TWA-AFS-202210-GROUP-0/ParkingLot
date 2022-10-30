10061136d2aa8f92a248553403b12e4a39f1add6
Mengyu <mhan8@slb.com>
2022-10-30 21:55:37 +0800

    feat: AC1 & AC2 in story1 finished in story 3

--- ParkingLot/ParkingBoy.cs
+++ ParkingLot/ParkingBoy.cs
@@ -8,19 +8,21 @@ namespace ParkingLot
 {
     public class ParkingBoy
     {
+        private const string PARKINGLOT1 = "ParkingLot1";
+        private const string PARKINGLOT2 = "ParkingLot2";
         private ParkingLot parkingLot1;
         private ParkingLot parkingLot2;
 
         public ParkingBoy()
         {
-            parkingLot1 = new ParkingLot("ParkingLot1");
-            parkingLot2 = new ParkingLot("ParkingLot2");
+            parkingLot1 = new ParkingLot(PARKINGLOT1);
+            parkingLot2 = new ParkingLot(PARKINGLOT2);
         }
 
         public ParkingBoy(int lot1Limit, int lot2Limit)
         {
-            parkingLot1 = new ParkingLot("ParkingLot1", lot1Limit);
-            parkingLot2 = new ParkingLot("ParkingLot2", lot2Limit);
+            parkingLot1 = new ParkingLot(PARKINGLOT1, lot1Limit);
+            parkingLot2 = new ParkingLot(PARKINGLOT2, lot2Limit);
         }
 
         public Ticket Park(Car car)
@@ -29,16 +31,28 @@ namespace ParkingLot
             {
                 return parkingLot2.BeParked(car);
             }
+
+            if (parkingLot2.IsAtCapacity())
+            {
+                return null;
+            }
+
             return parkingLot1.BeParked(car);
         }
 
-        public List<Ticket> Park(List<Car> car)
-        {
-            return null;
-        }
+        public List<Ticket> Park(List<Car> cars) => cars.Select(car => Park(car)).ToList();
 
         public Car Fetch(Ticket ticket)
         {
+            if (ticket.ParkingLot == PARKINGLOT1)
+            {
+                return parkingLot1.BeFetched(ticket);
+            }
+            else if (ticket.ParkingLot == PARKINGLOT2)
+            {
+                return parkingLot2.BeFetched(ticket);
+            }
+
             return null;
         }
     }
--- ParkingLotTest/ParkingBoyTest.cs
+++ ParkingLotTest/ParkingBoyTest.cs
@@ -26,5 +26,118 @@ namespace ParkingLotTest
             Assert.Equal("ParkingLot1", ticket1.ParkingLot);
             Assert.Equal("ParkingLot2", ticket2.ParkingLot);
         }
+
+        [Fact]
+        public void Should_get_parking_ticket_when_park_a_car_given_a_parking_lot_and_a_car()
+        {
+            // given
+            Car car = new Car() { Name = "Xiaohei", Owner = "laohei" };
+            ParkingBoy parkingBoy = new ParkingBoy(1, 2);
+
+            // when
+            var ticket = parkingBoy.Park(car);
+
+            // then
+            Assert.Equal(car, ticket.Car);
+        }
+
+        [Fact]
+        public void Should_get_parking_tickets_when_park_cars_given_a_parking_lot_and_cars()
+        {
+            // given
+            Car car1 = new Car() { Name = "Xiaohei", Owner = "Laohei" };
+            Car car2 = new Car() { Name = "Xiaobai", Owner = "Laobai" };
+            Car car3 = new Car() { Name = "Xiaohuang", Owner = "Laohuang" };
+            List<Car> cars = new List<Car>() { car1, car2, car3 };
+            ParkingBoy parkingBoy = new ParkingBoy(1, 2);
+
+            // when
+            var tickets = parkingBoy.Park(cars);
+
+            // then
+            Assert.Equal(tickets[0].Car, car1);
+        }
+
+        //[Fact]
+        //public void Should_NOT_get_CAR_when_park_cars_given_a_wrong_ticket()
+        //{
+        //    // given
+        //    Car car = new Car() { Name = "Xiaohei", Owner = "laohei" };
+        //    ParkingLot parkingLot = new ParkingLot("parkinglot");
+        //    parkingLot.BeParked(car);
+        //    var ticket = new Ticket() { Car = car };
+
+        //    // given
+        //    // then
+        //    Exception ex = Assert.Throws<Exception>(() => { parkingLot.BeFetched(ticket); });
+        //    Assert.Equal("Unrecognized parking ticket.", ex.Message);
+        //}
+
+        //[Fact]
+        //public void Should_NOT_get_CAR_when_park_cars_given_a_used_ticket()
+        //{
+        //    // given
+        //    Car car = new Car() { Name = "Xiaohei", Owner = "laohei" };
+        //    ParkingLot parkingLot = new ParkingLot("parkinglot");
+        //    var ticket = parkingLot.BeParked(car);
+        //    var parkedCar = parkingLot.BeFetched(ticket);
+        //    Assert.Equal(parkedCar, car);
+        //    Assert.True(ticket.IsUsed);
+
+        //    // when
+        //    // then
+        //    Exception ex = Assert.Throws<Exception>(() => { parkingLot.BeFetched(ticket); });
+        //    Assert.Equal("Unrecognized parking ticket.", ex.Message);
+        //}
+
+        //[Fact]
+        //public void Should_NOT_get_ticket_when_park_cars_given_parking_lot_is_at_capacity()
+        //{
+        //    // given
+        //    Car car1 = new Car() { Name = "Xiaohei", Owner = "Laohei" };
+        //    Car car2 = new Car() { Name = "Xiaobai", Owner = "Laobai" };
+        //    Car car3 = new Car() { Name = "Xiaohuang", Owner = "Laohuang" };
+        //    ParkingLot parkingLot = new ParkingLot("parkinglot", 2);
+        //    var ticket1 = parkingLot.BeParked(car1);
+        //    var ticket2 = parkingLot.BeParked(car2);
+        //    Assert.NotNull(ticket1);
+        //    Assert.NotNull(ticket2);
+
+        //    // when
+        //    // then
+        //    Exception ex = Assert.Throws<Exception>(() => { parkingLot.BeParked(car3); });
+        //    Assert.Equal("Not enough position.", ex.Message);
+        //}
+
+        //[Fact]
+        //public void Should_NOT_get_last_ticket_when_park_cars_given_parking_lot_is_at_capacity()
+        //{
+        //    // given
+        //    Car car1 = new Car() { Name = "Xiaohei", Owner = "Laohei" };
+        //    Car car2 = new Car() { Name = "Xiaobai", Owner = "Laobai" };
+        //    Car car3 = new Car() { Name = "Xiaohuang", Owner = "Laohuang" };
+        //    List<Car> cars = new List<Car>() { car1, car2, car3 };
+        //    ParkingLot parkingLot = new ParkingLot("parkinglot", 2);
+
+        //    // when
+        //    // then
+        //    Exception ex = Assert.Throws<Exception>(() => { parkingLot.BeParked(cars); });
+        //    Assert.Equal("Not enough position.", ex.Message);
+        //}
+
+        //[Fact]
+        //public void Should_NOT_get_CAR_when_fetch_car_given_no_ticket()
+        //{
+        //    // given
+        //    Car car = new Car() { Name = "Xiaohei", Owner = "laohei" };
+        //    ParkingLot parkingLot = new ParkingLot("parkinglot");
+        //    parkingLot.BeParked(car);
+        //    var ticket = new Ticket() { Car = car };
+
+        //    // when
+        //    // then
+        //    Exception ex = Assert.Throws<Exception>(() => { parkingLot.BeFetched(null); });
+        //    Assert.Equal("Please provide your parking ticket.", ex.Message);
+        //}
     }
 }
