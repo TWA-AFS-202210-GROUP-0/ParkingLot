e0f861102e7f5c6f040356fe9b3893288dbb2ed3
Jie Li <JLi147@slb.com>
2022-10-30 18:22:54 +0800

    feat: add ParkingBoy parking a car and return a ticket

---
+++ ParkingLotTest/ParkingBoyTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingLot;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class ParkingBoyTest
+    {
+        [Fact]
+        public void Shoule_return_ticket_when_parkingBoy_park_car_given_a_parkingBoy_and_a_car()
+        {
+            // given
+            ParkingBoy parkingBoy = new ParkingBoy();
+            Car car = new Car();
+            // when
+            var ticket = parkingBoy.Park(car);
+            // then
+            Assert.Equal(typeof(Ticket), ticket.GetType());
+        }
+    }
+}
